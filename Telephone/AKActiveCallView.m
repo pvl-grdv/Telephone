21f143ca28c32b825d5cd08b2bf55f0d7172a7ab
Pavel Gordeev <830451+pvl-grdv@users.noreply.github.com>
2026-09-22 13:41:48 +0400

    Simplify active call key handling

--- Telephone/AKActiveCallView.m
+++ Telephone/AKActiveCallView.m
@@ -26,27 +26,23 @@
 }
 
 - (void)keyDown:(NSEvent *)theEvent {
-    NSCharacterSet *DTMFCharacterSet = [NSCharacterSet characterSetWithCharactersInString:@"0123456789*#abcdrABCDR"];
-    NSCharacterSet *commandsCharacterSet = [NSCharacterSet characterSetWithCharactersInString:@"mh"];
-    
-    unichar firstCharacter = [[theEvent characters] characterAtIndex:0];
-    if ([DTMFCharacterSet characterIsMember:firstCharacter]) {
-        if (![theEvent isARepeat]) {
-            // We want to get DTMF string as text.
-            [self interpretKeyEvents:@[theEvent]];
-        }
-    } else if ([commandsCharacterSet characterIsMember:firstCharacter]) {
-        if (![theEvent isARepeat]) {
-            // Pass call control commands further so that main menu will catch them.
-            // The corresponding key equivalents must be set in the main menu.
-            // We need this because we have key equivalents without modifiers,
-            // in which case NSApplication can't recognize them and can't dispatch
-            // appropriate events before we even get here.
-            [super keyDown:theEvent];
-        }
-    } else {
+    NSString *characters = theEvent.characters;
+    if (characters.length == 0) {
         [super keyDown:theEvent];
+        return;
     }
+
+    NSCharacterSet *DTMFCharacterSet =
+        [NSCharacterSet characterSetWithCharactersInString:@"0123456789*#abcdrABCDR"];
+    unichar firstCharacter = [characters characterAtIndex:0];
+
+    if ([DTMFCharacterSet characterIsMember:firstCharacter] && !theEvent.isARepeat) {
+        // We want to get DTMF input as text so the delegate can forward it.
+        [self interpretKeyEvents:@[theEvent]];
+        return;
+    }
+
+    [super keyDown:theEvent];
 }
 
 - (void)insertText:(id)aString {

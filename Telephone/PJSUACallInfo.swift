79c9428874c45655a54edde0034bc27773d87507
Pavel Gordeev <830451+pvl-grdv@users.noreply.github.com>
2026-09-27 00:45:44 +0400

    Implement PJSIP call snapshot in Swift

---
+++ Telephone/PJSUACallInfo.swift
@@ -0,0 +1,54 @@
+//
+//  PJSUACallInfo.swift
+//  Telephone
+//
+//  Safe snapshot of the PJSIP call information used by the app.
+//
+
+import Foundation
+
+@objc @implementation
+extension PJSUACallInfo {
+    public let identifier: Int
+    public let accountIdentifier: Int
+    public let state: AKSIPCallState
+    public let stateText: String
+    public let lastStatus: Int
+    public let lastStatusText: String
+    public let localURI: AKSIPURI
+    public let remoteURI: AKSIPURI
+    public let isIncoming: Bool
+
+    public init(
+        info: pjsua_call_info,
+        parser: AKSIPURIParser
+    ) {
+        identifier = Int(info.id)
+        accountIdentifier = Int(info.acc_id)
+        state = AKSIPCallState(
+            rawValue: UInt(info.state.rawValue)
+        )!
+        stateText = swiftString(info.state_text)
+        lastStatus = Int(info.last_status)
+        lastStatusText = swiftString(info.last_status_text)
+        localURI = parser.sipURI(from: swiftString(info.local_info))
+            ?? AKSIPURI()
+        remoteURI = parser.sipURI(from: swiftString(info.remote_info))
+            ?? AKSIPURI()
+        isIncoming = info.role == PJSIP_ROLE_UAS
+        super.init()
+    }
+}
+
+private func swiftString(_ value: pj_str_t) -> String {
+    guard let pointer = value.ptr, value.slen > 0 else {
+        return ""
+    }
+
+    return String(
+        bytesNoCopy: UnsafeMutableRawPointer(pointer),
+        length: Int(value.slen),
+        encoding: .utf8,
+        freeWhenDone: false
+    ) ?? ""
+}

7dc70f662bf523ed9b97a2d6504412b152d34ca1
Pavel Gordeev <830451+pvl-grdv@users.noreply.github.com>
2026-09-27 01:20:13 +0400

    Expose PJSIP credential error to Swift

--- Telephone/PJSUACallbacks.h
+++ Telephone/PJSUACallbacks.h
@@ -30,3 +30,7 @@ void PJSUAOnCallReplaced(pjsua_call_id oldCallID, pjsua_call_id newCallID);
 void PJSUAOnAccountRegistrationState(pjsua_acc_id accountID);
 void PJSUAOnNATDetect(const pj_stun_nat_detect_result *result);
 void PJSUAOnAccountFindForIncoming(const pjsip_rx_data *rdata, pjsua_acc_id *acc_id);
+
+static inline pj_status_t TelephonePJSIPFailedCredentialError(void) {
+    return PJSIP_EFAILEDCREDENTIAL;
+}
